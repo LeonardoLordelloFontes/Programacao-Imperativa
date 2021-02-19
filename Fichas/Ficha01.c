ddf59ab9c3bf33475582c4ea303bb03092e0c0dd
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2021-02-19 23:46:35 +0000

    Add files via upload

---
+++ Fichas/Ficha01.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Exercício 3;
+
+// 3.1;
+
+void quadrado (int n) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            putchar('#');
+        }
+        putchar('\n');
+    }
+
+}
+
+// 3.2
+
+void quadrado2 (int n) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (((i + j) % 2) == 0)
+                putchar('#');
+            else
+                putchar('_');
+        }
+        putchar('\n');
+    }
+
+}
+
+// 3.3
+
+void triangulo (int n) {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            putchar('#');
+        }
+
+        putchar('\n');
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            putchar('#');
+        }
+        putchar('\n');
+    }
+
+} 
+
+// 3.3
+
+void triangulo2(int n) {
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = n - 1; j > i ; j--) {
+            putchar(' ');
+        }
+
+        for (int k = 1; k < 2 * (i + 1); k++) {
+            putchar('#');
+        }
+
+        putchar('\n');
+
+    }
+
+}
+
+
+int main() {
+
+    triangulo2(30);
+
+    return 0;
+}
